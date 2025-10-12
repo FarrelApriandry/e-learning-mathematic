7aab8e44c229bd044b3663091c339199ccd3e03a
FarrelApri <fapriandry@gmail.com>
2025-10-12 19:16:44 +0700

    Switch Astro adapter from Node to Vercel
    Replaces the Node adapter with the Vercel serverless adapter in astro.config.mjs and updates dependencies accordingly. This change prepares the project for deployment on Vercel's serverless platform.

--- my-matematika/astro.config.mjs
+++ my-matematika/astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 // import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
     output: 'server',
-    adapter: node({
-        mode: 'standalone',
-    }),
+    adapter: vercel({}),
+    // experimental: {
+    //     serverIslands: true,
+    // },
     // integrations: [react()],
 });
