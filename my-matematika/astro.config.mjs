8ac4f0a1f99ef5cec1adb2f3c7f458586dd8ded0
FarrelApri <fapriandry@gmail.com>
2025-10-16 01:19:33 +0700

    Update Vercel adapter import in Astro config
    Changed the Vercel adapter import from '@astrojs/vercel/serverless' to '@astrojs/vercel' in astro.config.mjs to use the latest recommended package.

--- my-matematika/astro.config.mjs
+++ my-matematika/astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import react from "@astrojs/react";
 // import fs from "@astrojs/fs";
 // import react from '@astrojs/react';
